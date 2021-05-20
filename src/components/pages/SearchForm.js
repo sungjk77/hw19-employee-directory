406678325fd11cf4ea0c73a40f29fbfaa666ab0e
Sung Kim <sungjk77@msn.com>
2021-05-20 16:49:12 -0700

    cleaned up code

--- src/components/pages/SearchForm.js
+++ src/components/pages/SearchForm.js
@@ -4,36 +4,28 @@ export default function SearchForm(props) {
   const filtered = props;
   console.log(filtered)
   return (
-    <form className="d-flex px-3">
-      {/* <input
-          onChange={props.handleFilterBy}
-          value={props.search}
-          name="search"
-          type="text"
-          placeholder="Search For an Employee"
-          id="search"
-          className="form-control px-1 mx-1"
-          list="people"
+    <form className="d-flex px-3 my-3">
+      <input
+        value={props.search}
+        onChange={props.handleInputChange}
+        name="search"
+        list="employee"
+        type="text"
+        className="form-control px-1 mx-1"
+        placeholder="Search For an Employee"
+         id="search"
       />
+      <datalist id="employee">
+        {/* {props.employee.map(employee => (
+          <option value={employee.name} key={employee} />
+        ))} */}
+      </datalist>
+      {/* 
       <datalist id="people">
       {/* {filtered.employee.map(employee => (
         <option value={employee.email} key={employee.email} />))} 
       </datalist> */}
-          <input
-            value={props.search}
-            onChange={props.handleInputChange}
-            name="search"
-            list="employee"
-            type="text"
-            className="form-control"
-            placeholder="Search For an Employee"
-            id="filter"
-          />
-        <datalist id="employee">
-          {/* {props.employee.map(employee => (
-            <option value={employee.name} key={employee} />
-          ))} */}
-        </datalist>
+
     </form>
   );
 }
